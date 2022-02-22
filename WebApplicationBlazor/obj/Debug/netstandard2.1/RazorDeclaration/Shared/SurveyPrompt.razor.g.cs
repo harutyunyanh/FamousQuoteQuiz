77da01d9c55a9e30e46f9498d5c21384a0565445
Hovhannes <hovhannes@applebrie.com>
2022-02-22 17:02:57 +0400

    init

--- WebApplicationBlazor/obj/Debug/netstandard2.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ WebApplicationBlazor/obj/Debug/netstandard2.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,63 +13,63 @@ namespace WebApplicationBlazor.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 1 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 2 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 3 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 4 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 5 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 6 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 7 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 8 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using WebApplicationBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
+#line 9 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\_Imports.razor"
 using WebApplicationBlazor.Shared;
 
 #line default
@@ -83,7 +83,7 @@ using WebApplicationBlazor.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\Hovhannes\source\repos\FamousQuoteQuiz\WebApplicationBlazor\Shared\SurveyPrompt.razor"
+#line 12 "D:\Gits\FamousQuoteQuiz\WebApplicationBlazor\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
